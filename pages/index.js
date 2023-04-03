a1380716767287e58b0a9337e73f139e4822c8df
VikingHoard <91062510+VikingHoard@users.noreply.github.com>
2023-04-03 15:55:57 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,19 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@algolia/algoliasearch-netlify-frontend@1/dist/algoliasearchNetlify.css" />
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/@algolia/algoliasearch-netlify-frontend@1/dist/algoliasearchNetlify.js"></script>
+<script type="text/javascript">
+  algoliasearchNetlify({
+    appId: '8VDJOUIEOJ',
+    apiKey: '1b1a551db869f7a5e9eb341f9706631d',
+    siteId: '4a4b6f4b-2416-48e6-a343-683e915e9cdf',
+    branch: 'main',
+    selector: 'div#search',
+  });
+</script>
+
+
 export default function Home() {
   return (
     <div className="container">
@@ -11,6 +24,9 @@ export default function Home() {
       </Head>
 
       <main>
+	    <div id="search>
+		SEARCH DIV
+		</div>
         <Header title="Welcome to my app!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
